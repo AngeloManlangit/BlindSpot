02b78a740303d109c5c3a9ad1a1c826802c39828
Angelo Manlangit <manlangitangelomh@gmail.com>
2026-02-23 16:09:52 +0800

    Fixed merge bugs

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,24 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 import LandingView from '@/components/LandingView.vue'
 
-
-const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'LandingView',
-    component: LandingView,
-  }
-]
-
 // views
-import Landing from "@/views/Landing.vue"
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      component: Landing
+      component: LandingView
     }
   ],
 })
